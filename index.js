b6c7e3c1f9ed77cdeb3eabc7bc4e4773c3393f16
José Arturo Chávez Yzquierdo <kingofcode@kali.arturo>
2022-03-22 23:50:51 -0600

    primer funcionamiento dinamico V 0.5.0

--- index.js
+++ index.js
@@ -4,19 +4,24 @@ document.addEventListener('DOMContentLoaded', function () {
 	const Descripcion_tarea = document.getElementById('description');
 	const boton_add = document.getElementById('add');
 	const Tabla = document.getElementById('table');
+	const alert = document.getElementById('alert');
 
-	boton_add.onclick = function () {
-		console.log('title: ', Titulo_tarea.value);
-		console.log('description: ', Descripcion_tarea.value);
-		add_todo()
-	};
+	boton_add.onclick = add_todo;
 
 	function add_todo () {
+		
+		console.log('title: ', Titulo_tarea.value);
+		console.log('description: ', Descripcion_tarea.value);
+		
 		if (Titulo_tarea.value === '' ||  Descripcion_tarea.value === '') {
 			console.error('faltan parametros en titulo y descripcion')
+			alert.classList.remove('d-none')
+			alert.innerText = 'faltan parametros en titulo y descripcion'
 		}else{
 			Titulo_tarea.value = ""
 			Descripcion_tarea.value = ""
+			alert.classList.add('d-none')
+			alert.innerText = ''
 		}
 	};
 });
